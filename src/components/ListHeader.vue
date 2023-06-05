18ec4c8c64afdb90a1b63e00fbb6a138ac479ca8
miao.zhang <miao.zhang@thoughtworks.com>
2023-06-05 21:49:19 +0800

    feat:todo list css

--- src/components/ListHeader.vue
+++ src/components/ListHeader.vue
@@ -1,9 +1,9 @@
 <template>
-  <div>
+  <div class="list-header">
     <div class="title">Use this manage your life and work</div>
     <div class="justify">
-      <el-input class="input" placeholder="Enter your to do item" v-model="input" />
-      <el-button :icon="Plus" @click="$emit('addTodoItem', input)"/>
+      <input class="input" placeholder="Enter your to do item" v-model="input"/>
+      <el-button class="button" :icon="Plus" @click="$emit('addTodoItem', input)"/>
     </div>
   </div>
 </template>
@@ -14,10 +14,41 @@ import { ref } from 'vue'
 const input = ref('')
 </script>
 <style scoped>
+.list-header {
+  display:flex;
+  flex-direction: column;
+  text-align: center;
+  padding: 0 40px;
+}
 .title {
   font-size: 20px;
   font-weight: bold;
   font-style: italic;
   color: #343a40;
+  margin: 16px 0;
+}
+.input {
+  font-size: 20px;
+  border: 1px solid #a9a9a9;
+  border-radius: 3px;
+  flex: 1;
+  margin-right: 8px;
+  padding: 0  16px;
+}
+.input:focus {
+  outline: none;
+  border: 1px solid #7dced5;
+}
+.button {
+  background-color: #7dced5;
+  width: 48px;
+  height: 48px;
+  color: white;
+  font-size: 32px;
+  border-radius: 3px;
+}
+.button:active {
+  background-color: #31c5d3;
+  border: none;
 }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,10 @@
 <template>
-  <div>
+  <div class="list">
     <el-checkbox-group v-model="checkList">
       <div v-for="item in props.todoList">
-        <div  v-show="item.status">
-          <el-checkbox :key="item.id" :label="item.name" />
-          <Delete class="delete-icon" @click="$emit('deleteItem',item.id)"/>
+        <div v-show="item.status" class="list-item">
+          <el-checkbox class="checkbox" :key="item.id" :label="item.name" />
+          <Delete class="delete-icon" @click="$emit('deleteItem', item.id)" />
         </div>
       </div>
     </el-checkbox-group>
@@ -25,6 +25,38 @@ const props = defineProps<{
 .delete-icon {
   height: 16px;
   width: 16px;
-  color: brown;
+  color: #f56969;
+  display: none;
+}
+.list {
+  padding: 16px 40px;
+  min-height: 140px;
+  overflow: auto;
+}
+.checkbox {
+  padding-left: 16px;
+}
+.el-checkbox__label {
+  text-overflow: ellipsis;
+  max-width: 540px;
+  overflow: hidden;
+}
+.list-item {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  background-color: #f9f9f9;
+  height: 40px;
+  margin-bottom: 8px;
+  border-radius: 3px;
+  text-overflow: ellipsis;
+}
+.list-item:hover {
+  background-color: #dfdfdf;
+}
+.list-item:hover .delete-icon {
+  display: inline;
+  padding-right: 16px;
+  padding-left: 8px;
 }
 </style>
